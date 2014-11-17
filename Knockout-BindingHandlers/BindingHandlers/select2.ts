bd17b17659df040712420dbf2271db9dafbb915e
Nicolas Hémonic <nicolas.hemonic@gmail.com>
2014-11-17 22:35:26 +0100

    Added select2 binding handler

--- Knockout-BindingHandlers/BindingHandlers/select2.ts
+++ Knockout-BindingHandlers/BindingHandlers/select2.ts
@@ -2,44 +2,77 @@
     select2: KnockoutBindingHandler;
 }
 
-ko.bindingHandlers.select2 = {
+module BindingHandlers {
 
-    init: (el, valueAccessor, allBindingsAccessor, viewModel) => {
-        ko.utils.domNodeDisposal.addDisposeCallback(el, () => {
-            $(el).select2('destroy');
-        });
+    ko.bindingHandlers.select2 = {
 
-        var allBindings = allBindingsAccessor(),
-            select2 = ko.utils.unwrapObservable(allBindings.select2);
-
-        $(el).select2(select2);
-    },
-
-    update: (el, valueAccessor, allBindingsAccessor, viewModel) => {
-        var allBindings = allBindingsAccessor();
-
-        if ("value" in allBindings) {
-            $(el).select2("val", allBindings.value());
-        } else if ("selectedOptions" in allBindings) {
-            var converted = [];
-            var textAccessor = (value) => { return value; };
-            if ("optionsText" in allBindings) {
-                textAccessor = (value) => {
-                    var valueAccessor = (item) => { return item; }
-                    if ("optionsValue" in allBindings) {
-                        valueAccessor = (item) => { return item[allBindings.optionsValue]; }
-                    }
-                    var items = $.grep(allBindings.options(), (e) => { return valueAccessor(e) == value });
-                    if (items.length == 0 || items.length > 1) {
-                        return "UNKNOWN";
-                    }
-                    return items[0][allBindings.optionsText];
-                }
-            }
-            $.each(allBindings.selectedOptions(), (key, value) => {
-                converted.push({ id: value, text: textAccessor(value) });
+        init: (el, valueAccessor, allBindingsAccessor, viewModel) => {
+            ko.utils.domNodeDisposal.addDisposeCallback(el, () => {
+                $(el).select2('destroy');
             });
-            $(el).select2("data", converted);
+
+            var allBindings = allBindingsAccessor(),
+                select2 = ko.utils.unwrapObservable(allBindings.select2);
+
+            $(el).select2(select2);
+        },
+
+        update: (el, valueAccessor, allBindingsAccessor, viewModel) => {
+            var allBindings = allBindingsAccessor();
+
+            if ("value" in allBindings) {
+                $(el).select2("val", allBindings.value());
+
+                var selectedOption;
+                for (var i = 0; i < allBindings.options().length; i++) {
+                    if (allBindings.options()[i].id == allBindings.value()) {
+                        selectedOption = allBindings.options()[i];
+                    }
+                }
+                allBindings.select2.value(selectedOption);
+                console.log(selectedOption);
+
+            } else if ("selectedOptions" in allBindings) {
+                var converted = [];
+                var textAccessor = (value) => { return value; };
+
+                var selectedOptions = [];
+                for (var i = 0; i < allBindings.options().length; i++) {
+                    for (var j = 0; j < allBindings.selectedOptions().length; j++) {
+                        if (allBindings.options()[i].id == allBindings.selectedOptions()[j]) {
+                            selectedOptions.push(allBindings.options()[i]);
+                        }
+                    }
+                }
+                allBindings.select2.value(selectedOptions);
+                console.log(selectedOptions);
+
+                // if options are object
+                // the text/value could be changed
+                if ("optionsText" in allBindings) {
+                    textAccessor = (value) => {
+                        var valueAccessor = (item) => { return item; }
+                        if ("optionsValue" in allBindings) {
+                            valueAccessor = (item) => {
+                                return item[allBindings.optionsValue];
+                            }
+                        }
+                        var items = $.grep(allBindings.options(), (e) => {
+                            return valueAccessor(e) == value
+                        });
+                        if (items.length == 0 || items.length > 1) {
+                            return "UNKNOWN";
+                        }
+                        return items[0][allBindings.optionsText];
+                    }
+                }
+
+                $.each(allBindings.selectedOptions(), (key, value) => {
+                    converted.push({ id: value, text: textAccessor(value) });
+                });
+                $(el).select2("data", converted);
+            }
         }
-    }
-};
+    };
+
+}
--- Knockout-BindingHandlers/ViewModels/test.ts
+++ Knockout-BindingHandlers/ViewModels/test.ts
@@ -1,31 +1,27 @@
-﻿function format(state) {
-    return state.text.toUpperCase();
-}
-
-class State {
+﻿class State {
     constructor(public id, public text) {
         this.id = id;
         this.text = text;
-    }
-    public toString () {
-        return this.text + "(" + this.id + ")";
     }
 }
 
 class ViewModel {
     constructor(){
-        this.states = [
+        this.states = ko.observableArray<State>([
             new State("AL", "Alabama"),
             new State("AK", "Alaska"),
             new State("AZ", "Arizona"),
             new State("WV", "West Virginia"),
             new State("WI", "Wisconsin"),
             new State("WY", "Wyoming")
-        ];
+        ]);
     }
     
     private states;
-    private selectedState = ko.observable<string>("(none)");
+    private selectedState = ko.observable<string>();
+    private selectedStateObj = ko.observable<State>();
+    private selectedStates = ko.observableArray<string>([]);
+    private selectedStatesObj = ko.observableArray<State>([]);
 };
 
 $(document).ready(() => {
