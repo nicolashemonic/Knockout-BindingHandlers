ecb37e7c8e80ace3bcb71fd7ec1a51104656c948
Nicolas Hémonic <nicolas.hemonic@gmail.com>
2015-03-04 18:04:32 +0100

    Add Select2 multiple and improvements

--- Knockout-BindingHandlers/BindingHandlers/select2.ts
+++ Knockout-BindingHandlers/BindingHandlers/select2.ts
@@ -9,30 +9,53 @@ module BindingHandlers {
         init: (element, valueAccessor, allBindingsAccessor, viewModel) => {
             var select2 = valueAccessor();
             var value = ko.utils.unwrapObservable(select2.value);
+            var isEnabled = ko.utils.unwrapObservable(select2.enable);
 
             ko.utils.domNodeDisposal.addDisposeCallback(element,() => {
                 jQuery(element).select2('destroy');
             });
 
+            // unwrap data from observable for select2 constructor
             select2.data = ko.utils.unwrapObservable(select2.data);
 
             jQuery(element).select2(select2);
 
+            // set defaults
             if (value) {
                 jQuery(element).select2("data", value);
             }
+            if (isEnabled === false) {
+                jQuery(element).select2("enable", isEnabled);
+            }
 
-            jQuery(element).on("change", (data) => {
-                select2.value(data.added); // update observable from select2
+            jQuery(element).on("change",(data) => {
+                if (select2.multiple) { // update observable array from select2
+                    if (data.added) {
+                        select2.value.push(data.added); 
+                    } else {
+                        select2.value.remove(data.removed);
+                    }
+                } else { // update observable from select2
+                    if (data.added) {
+                        select2.value(data.added);
+                    } else {
+                        select2.value(undefined);
+                    }
+                }
             });
         },
 
         update: (element, valueAccessor, allBindingsAccessor, viewModel) => {
             var select2 = valueAccessor();
             var value: any = ko.utils.unwrapObservable(select2.value);
+            var isEnabled = ko.utils.unwrapObservable(select2.enable);
             
-            if (value) {
-                jQuery(element).select2("data", value); // update select2 from observable
+            jQuery(element).select2("data", value); // update select2 from observable
+
+            if (isEnabled === false) {
+                jQuery(element).select2("enable", isEnabled);
+            } else {
+                jQuery(element).select2("enable", true);
             }
         }
     };
--- Knockout-BindingHandlers/ViewModels/test.ts
+++ Knockout-BindingHandlers/ViewModels/test.ts
@@ -1,47 +1,121 @@
 ﻿class State {
+    constructor(public id, public text) {
+        this.id = id;
+        this.text = text;
+    }
+}
+
+class CustomIdState {
     constructor(public value, public text) {
         this.value = value;
         this.text = text;
     }
 }
 
+class CustomTextState {
+    constructor(public id, public display) {
+        this.id = id;
+        this.display = display;
+    }
+}
+
 var json = [
-    new State(1, "Alabama"),
-    new State("AK", "Alaska"),
-    new State("AZ", "Arizona"),
+    // integer ID
+    new State(1, "Alabama"), 
+    // litteral object
+    { id: "AK", text: "Alaska" }, 
+    // constructor objects 
+    new State("AZ", "Arizona"), 
     new State("WV", "West Virginia"),
     new State("WI", "Wisconsin"),
     new State("WY", "Wyoming")
 ];
 
-class ViewModel {
-    constructor() {
-        setTimeout(() => {
-            this.states(json);
-        }, 500);
+var customIdJson = [
+    // integer ID
+    new CustomIdState(1, "Alabama"), 
+    // litteral object
+    { value: "AK", text: "Alaska" }, 
+    // constructor objects 
+    new CustomIdState("AZ", "Arizona"),
+    new CustomIdState("WV", "West Virginia"),
+    new CustomIdState("WI", "Wisconsin"),
+    new CustomIdState("WY", "Wyoming")
+];
 
-        setTimeout(() => {
-            this.selectedState(json[4]);
-        }, 1000);
+var customTextJson = [
+    // integer ID
+    new CustomTextState(1, "Alabama"), 
+    // litteral object
+    { id: "AK", display: "Alaska" }, 
+    // constructor objects 
+    new CustomTextState("AZ", "Arizona"),
+    new CustomTextState("WV", "West Virginia"),
+    new CustomTextState("WI", "Wisconsin"),
+    new CustomTextState("WY", "Wyoming")
+];
+
+class ViewModel {
+    constructor(scope: HTMLElement) {
+        ko.applyBindings(this, scope);
+        this.states(json);
+        this.customIdStates(customIdJson);
+        this.customTextStates(customTextJson);
     }
 
     private states = ko.observableArray<State>([]);
+    private customIdStates = ko.observableArray<CustomIdState>([]);
+    private customTextStates = ko.observableArray<CustomTextState>([]);
 
     private selectedState = ko.observable<State>();
+    private selectedCustomIdState = ko.observable<CustomIdState>();
+    private selectedCustomTextState = ko.observable<CustomTextState>();
+    private selectedStates = ko.observableArray<State>([]);
+
+    private isEnabled = ko.observable<boolean>(false);
 
     private getStates = () => {
         return { results: this.states() };
+    }
+
+    private getCustomIdStates = () => {
+        return { results: this.customIdStates() };
+    }
+
+    private getCustomTextStates = () => {
+        return { results: this.customTextStates() };
     }
 
     private getStateId = (obj) => {
         return obj.value;
     }
 
+    private getStateText = (obj) => {
+        return obj.display;
+    }
+
     private selectFirstState() {
         this.selectedState(this.states()[0]);
     }
+
+    private selectFirstCustomIdState() {
+        this.selectedCustomIdState(this.customIdStates()[0]);
+    }
+
+    private selectFirstCustomTextState() {
+        this.selectedCustomTextState(this.customTextStates()[0]);
+    }
+
+    private toggleIsEnabled() {
+        if (this.isEnabled()) {
+            this.isEnabled(false);
+        } else {
+            this.isEnabled(true);
+        }
+    }
 };
 
-$(document).ready(() => {
-    ko.applyBindings(new ViewModel());
+jQuery(document).ready(() => {
+    var scope = document.getElementById("scope");
+    var viewModel = new ViewModel(scope);
 });
